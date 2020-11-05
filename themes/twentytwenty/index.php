55f01e554ac553594a4d7890af0e144d99b9630d
Linus Kallin <jaglinus@gmail.com>
2020-11-05 10:12:45 +0100

    dfgöldfögl

--- themes/twentytwenty/index.php
+++ themes/twentytwenty/index.php
@@ -8,7 +8,7 @@
  * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
+ * dfgkldkfglkdlfkglkdflgklkdflgkdfg
  * @package WordPress
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
