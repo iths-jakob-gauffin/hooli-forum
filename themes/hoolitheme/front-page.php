ac3b0e7aa976d3a4479b157fca318d6d37035336
iths-jakob-gauffin <54362410+iths-jakob-gauffin@users.noreply.github.com>
2020-11-10 21:33:05 +0100

    change 1rem to 10px

--- themes/hoolitheme/front-page.php
+++ themes/hoolitheme/front-page.php
@@ -7,7 +7,7 @@
             // echo the_title();
             
             ?>  
-            <h1>test</h1>
+            <h1 style="font-size: 1rem">test</h1>
                 <div class="main-container">
                     <main class="content">
                         <?php echo the_content(); ?>
