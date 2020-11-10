4165b39bd85ed3c316160f45549da5cf6575a9a6
iths-jakob-gauffin <54362410+iths-jakob-gauffin@users.noreply.github.com>
2020-11-10 09:43:09 +0100

    add the content to front-apge

--- themes/hoolitheme/front-page.php
+++ themes/hoolitheme/front-page.php
@@ -1,4 +1,19 @@
 <?php get_header(); ?> 
 
+    <?php 
+        while(have_posts()){
+            the_post();
+
+            // echo the_title();
+            
+            ?>  
+                <div class="main-container">
+                    <main class="content">
+                        <?php echo the_content(); ?>
+                    </main>
+                </div>
+            <?php
+        }
+    ?>
 
 <?php get_footer(); ?>
