a72df2c98b3b334767238b4846cfc977779bd37c
Linus Kallin <jaglinus@gmail.com>
2020-11-11 11:56:12 +0100

    header

--- themes/hoolitheme/header.php
+++ themes/hoolitheme/header.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-  Header
-=======
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -46,4 +37,3 @@
         </header>
     </div>
     
->>>>>>> main
