59edc35e09910ac91e4874f16414617e4566c2b9
iths-jakob-gauffin <54362410+iths-jakob-gauffin@users.noreply.github.com>
2020-11-11 13:38:40 +0100

    Fix faulty syntax

--- themes/hoolitheme/functions.php
+++ themes/hoolitheme/functions.php
@@ -16,11 +16,10 @@ function hooliScripts(){
     $url = home_url( add_query_arg( null, null ));
 
     if(is_front_page() OR $url === site_url('community/') OR $url === site_url('community/senaste-inlaggen/') ){
-    {
         wp_register_style('extraStyle', get_template_directory_uri() . '/dist/extraStyle.css', [], 1, 'all');
         wp_enqueue_style('extraStyle');
     }
-    
+
 }
 
 add_action('wp_enqueue_scripts', 'hooliScripts');
