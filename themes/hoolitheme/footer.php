6428338e32aa3ba272aa430e0ee88d5ccaaab433
Jessica <jessica.schwabegger@hotmail.com>
2020-11-16 15:38:07 +0100

    Add footer

--- themes/hoolitheme/footer.php
+++ themes/hoolitheme/footer.php
@@ -1,3 +1,13 @@
         <?php wp_footer(); ?>
+        <div class="main-container">
+            <div class="footer">
+                <h3>
+                    <i class="far fa-copyright"></i>
+                    <i class="far fa-registered"></i>
+                    Hooli & Mikael Engström 2020
+                </h3>
+                <h3>kontakt@hooli.se  08-35 32 14</h3>
+            </div>
+        </div>
     </body>
 </html>
