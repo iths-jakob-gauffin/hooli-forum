df86acbc961194eaa9de705714be9f3f51df99a1
iths-linus-kallin <jaglinus@gmail.com>
2020-11-17 11:55:55 +0100

    move js function to separate file

---
+++ themes/hoolitheme/src/changeColorRatingBar.js
@@ -0,0 +1,14 @@
+function changeColors(){
+  let activeText = document.querySelector('.wpforo-active')
+  activeText.classList.add('.remove:hover')
+
+  let ratings = document.querySelectorAll('.rating-bar-cell')
+  ratings.forEach(element => {
+    if(!element.classList.contains("wpfbg-7")){
+      element.style.backgroundColor = "#7CC985"
+    } else {
+      element.style.backgroundColor = "#7CC985"
+    }
+  });
+}
+changeColors()
