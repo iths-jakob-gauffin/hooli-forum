f1941ddcd924f149417bbbe3ca873fd61700e08e
Jessica <jessica.schwabegger@hotmail.com>
2020-11-17 15:16:01 +0100

    Basic style to single.php

--- themes/hoolitheme/single.php
+++ themes/hoolitheme/single.php
@@ -8,14 +8,17 @@
 
         // echo $posta;
         
-        ?>
-            <h1>Här är single.php</h1>
-            <h2 class="HomePage__Title"><?php $titeln =  get_the_title(); echo $titeln; ?></h2> 
-            <p class="HomePage__Text"><?php echo get_the_content(); ?></p>   
-
-            <a href="<?php echo site_url('/blog');?>">Tillbaka till blog</a>
-            <a href="<?php echo site_url('/');?>">Tillbaka till hemsidan</a>
-
+        ?>  
+            <div class="main-container">
+                <div class="single">
+                    <h1 class="HomePage__Title"><?php $titeln =  get_the_title(); echo $titeln; ?></h1> 
+                    <p class="HomePage__Text"><?php echo get_the_content(); ?></p>   
+                    <div class="links">
+                        <a href="<?php echo site_url('/blog');?>">Tillbaka till blog</a>
+                        <a href="<?php echo site_url('/');?>">Tillbaka till hemsidan</a>
+                    </div>
+                </div>
+            </div>
         <?php
 
     }
