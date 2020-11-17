05b232793c98f74731ab0a4c81db968b7b68f34c
Jakob <jakob_gauffin@hotmail.com>
2020-11-17 13:37:57 +0100

    Fix searchglass size

--- themes/hoolitheme/templates/header-searchField.php
+++ themes/hoolitheme/templates/header-searchField.php
@@ -1,7 +1,7 @@
 <div class="Header__SearchField">
     <div class="wpf-search-form">
         <form class="Header__Form" action="<?php echo site_url('community'); ?>" method="get">
-            <input id="wpForoSearch" class="wpf-search-field Header__Input" name="wpfs" type="text" value="" style="margin-right:10px;">
+            <input id="wpForoSearch" class="wpf-search-field Header__Input" name="wpfs" type="text" value="" >
             <button type="submit" class="Header__SearchButton">
                     <i class="fas fa-search Header__Icon" aria="hidden"></i>
             </button>
