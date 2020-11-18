cf3ea560996f1c858a6da184e65864436d539781
Jessica <jessica.schwabegger@hotmail.com>
2020-11-18 11:45:41 +0100

    Update style on single.php

--- themes/hoolitheme/single.php
+++ themes/hoolitheme/single.php
@@ -12,6 +12,8 @@
             <div class="main-container">
                 <div class="single">
                     <h1><?php $titeln =  get_the_title(); echo $titeln; ?></h1>
+                    <p class="author">Skriven av <?php echo get_the_author(); ?></p>
+                    <div class="lines"></div>
                     <p><?php echo get_the_content(); ?></p>
                     <div class="links">
                         <a href="<?php echo site_url('/blog');?>">Tillbaka till bloggen</a>
