b1737215d5bd0b381983f7e6dfa3219419221fd7
Jakob <jakob_gauffin@hotmail.com>
2020-11-19 14:04:36 +0100

    Add gmaps install jquery

--- themes/hoolitheme/functions.php
+++ themes/hoolitheme/functions.php
@@ -4,6 +4,8 @@ function hooliScripts(){
     //fonts
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Varela+Round&display=swap', false);
 
+
+
     //font-awesome
     wp_enqueue_style('fontawesome5', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css', array(), null );
 
@@ -13,6 +15,9 @@ function hooliScripts(){
     wp_enqueue_script('jquery');
     wp_register_script('app', get_template_directory_uri() . '/dist/app.js', ['jquery'], 1, true);
     wp_enqueue_script('app');
+
+    //maps
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyBwDr8vH_NUbKigHt0TFX0Ezvq230Q6a5s', NULL, '1.0', true);
 
     // Add extra css on certain pages to remove unwanted breadcrumbs and title in wpForo    
     $url = home_url( add_query_arg( null, null ));
@@ -151,4 +156,10 @@ add_action('admin_init', 'redirectToFrontend');
     }
 
     add_action('pre_get_posts', 'queryPostsGetter');
+
+    function hooliMapKey(){
+        acf_update_setting('google_api_key', 'AIzaSyBwDr8vH_NUbKigHt0TFX0Ezvq230Q6a5s');
+    }
+    
+    add_filter('acf/init', 'hooliMapKey');
 
